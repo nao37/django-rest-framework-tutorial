34fcdf4c076a313c9bdcda0a5dac70c613c8674f
渡辺直 <w.nao.gh37@gmail.com>
2019-07-03 19:38:17 +0900

    db 設定の追記

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rest_framework',
+        'USER': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
