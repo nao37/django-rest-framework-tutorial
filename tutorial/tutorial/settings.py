7156646ef5cac0c2c379142ba7dfab2b728df931
渡辺直 <w.nao.gh37@gmail.com>
2019-07-04 17:47:40 +0900

    pagination 追記

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -107,7 +107,12 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+
+    # Pagination
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+
 }
 
 # Internationalization
