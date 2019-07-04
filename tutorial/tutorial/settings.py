2201a7d5f133c9c3aa46941d1761ff76cdc2ff01
渡辺直 <w.nao.gh37@gmail.com>
2019-07-04 18:37:27 +0900

    app から除外

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'quickstart.apps.QuickstartConfig',
+    # 'quickstart.apps.QuickstartConfig',
     'snippets.apps.SnippetsConfig',
 
 ]
