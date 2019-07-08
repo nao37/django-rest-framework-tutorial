a16a5a28301fcdda2c65b745f4c1cfa924b9eb46
渡辺直 <w.nao.gh37@gmail.com>
2019-07-08 15:56:39 +0900

    read only を削除

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -105,16 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # REST_FRAMEWORK settings
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ],
-
     # Pagination
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-
 }
 
 # Internationalization
