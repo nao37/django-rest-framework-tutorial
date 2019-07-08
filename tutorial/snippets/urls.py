b5081f96266560dfbdf440ee465a70e6bc96980b
渡辺直 <w.nao.gh37@gmail.com>
2019-07-08 14:52:00 +0900

    register urls into root urls

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from snippets import views
 
+app_name = 'snippets'
+
 urlpatterns = [
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
+    path('', views.snippet_list),
+    path('<int:pk>/', views.snippet_detail),
 ]
