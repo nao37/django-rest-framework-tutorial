9b54016bd121e6f398ce6bb497fd1d1183d6bf72
渡辺直 <w.nao.gh37@gmail.com>
2019-07-09 20:56:38 +0900

    user が入るようにした

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,13 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from snippets import views
 
-app_name = 'snippets'
-
 urlpatterns = [
-    path('', views.SnippetList.as_view(), name='snippet_list'),
-    path('<int:pk>/', views.SnippetDetail.as_view(), name='snippet_detail'),
+    path('snippets/', views.SnippetList.as_view(), name='snippet_list'),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet_detail'),
+    path('users/', views.UserList.as_view(), name='user_list'),
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='user_detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
