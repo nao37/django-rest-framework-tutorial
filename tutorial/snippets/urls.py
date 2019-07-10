407d5a1b35ec4bb19ddea1e51acec100b89b1e04
渡辺直 <w.nao.gh37@gmail.com>
2019-07-10 21:00:32 +0900

    root と hightlight url 設置

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -6,8 +6,10 @@ from snippets import views
 urlpatterns = [
     path('snippets/', views.SnippetList.as_view(), name='snippet_list'),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet_detail'),
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet_highlight'),
     path('users/', views.UserList.as_view(), name='user_list'),
     path('users/<int:pk>/', views.UserDetail.as_view(), name='user_detail'),
+    path('', views.api_root, name='root'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
