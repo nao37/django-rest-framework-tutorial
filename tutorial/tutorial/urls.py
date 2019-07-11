85002347a15d4843e63203e45979d8afe0835826
渡辺直 <w.nao.gh37@gmail.com>
2019-07-11 18:39:52 +0900

    不要部分の削除と schema の追記

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -15,19 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# from rest_framework import routers
-# from quickstart import views as qs_views
+from rest_framework.schemas import get_schema_view
 
-# # Routers provide an easy way of automatically determining the URL conf.
-# router = routers.DefaultRouter()
-# router.register(r'users', qs_views.UserViewSet)
-# router.register(r'groups', qs_views.GroupViewSet)
+schema_view = get_schema_view(title='Pastebin API')
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', include('snippets.urls')),
-    # path('', include(router.urls)),
+    path('schema/', schema_view),
 ]
