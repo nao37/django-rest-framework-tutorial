4d2f3f10749f0eee28e23f30b13c84e9873cba37
渡辺直 <w.nao.gh37@gmail.com>
2019-08-09 17:03:19 +0900

    STATIC_ROOT 設定

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -126,4 +126,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 STATIC_URL = '/static/'
